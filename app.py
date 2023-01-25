50dce831403eb6e47cdd69af9e5f87c4def76b9c
rimple882 <123411640+rimple882@users.noreply.github.com>
2023-01-25 02:37:37 +0200

    change

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ socketio = SocketIO(app,async_mode='gevent')
 
 @socketio.on('connect')
 def handle_connect(sid, environ):
-    print(0)
     print(sid, 'connected')
+    print(0)
 
 @socketio.on('disconnect')
 def handle_disconnect(sid):
